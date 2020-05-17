23a1f8ab0e17dee4bc0e3976354e97299e09cb33
Test-web-android <tomchang.us@outlook.com>
2020-05-17 10:53:01 -0700

    app-routing completed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackendService } from './backend.service';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactDetailComponent } from './contact-detail/contact-detail.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, ContactListComponent, ContactDetailComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    InMemoryWebApiModule.forRoot(BackendService),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/contact.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root'
+})
+export class ContactService {
+  API_URL: string = '/api/';
+  constructor(private http: HttpClient) { }
+  getContacts() {
+    return this.http.get(this.API_URL + 'contacts')
+  }
+
+  getContact(ContactId) {
+    return this.http.get(`${this.API_URL + 'contacts'}/${ContactId}`)
+  }
+}
